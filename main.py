81b18fd5f33d7c5c766c991eb61c295a790de081
“firstname <21mx201@psgtech.ac.in>
2022-08-01 22:45:42 +0530

    #COMMIT 5 *BY INCREASING SANKE LENGTH *AND DISPLAYING SCORE
    *(PROBLEM WITH COORDINATES)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ctypes.wintypes import SIZE
+from tkinter import font
 import pygame
 from sympy import true
 from pygame.locals import *
@@ -19,8 +20,8 @@ class Apple():
         pygame.display.flip()
 
     def move(self):
-        self.x=random.randint(0,32)*SIZE
-        self.Y=random.randint(0,16)*SIZE
+        self.x=random.randint(2,30)*SIZE
+        self.Y=random.randint(5,16)*SIZE
 
 
 
@@ -32,10 +33,16 @@ class Snake:
         self.block=pygame.image.load("resources/block.jpg").convert()#convert is the method for loading image
         #4
         self.length=length
-        self.x=[SIZE]*length
-        self.y=[SIZE]*length #these two are the position of the block
+        self.x=[40]*length
+        self.y=[40]*length #these two are the position of the block
         #3
         self.directions='down'
+
+
+    def inclength(self):
+        self.length+=1
+        self.x.append(1)
+        self.y.append(1)
         
     
     def draw(self):
@@ -44,11 +51,12 @@ class Snake:
             self.parent_screen.blit(self.block,(self.x[i],self.y[i]))
         pygame.display.flip()
 
+    
+
     def moveleft(self):
         self.directions='left'
     def moveright(self):
         self.directions='right'
-        
     def moveup(self):
         self.directions='up'
     def movedown(self):
@@ -83,7 +91,7 @@ class Game:
         pygame.init()
         self.surface=pygame.display.set_mode((1300,650)) # we are using sef here to make surface variable as class member so that we can use it in code later part.
         self.surface.fill((255,255,255))#were are making surface as class member
-        self.snake=Snake(self.surface,2)
+        self.snake=Snake(self.surface,1)
         self.snake.draw()
         self.apple=Apple(self.surface)
         self.apple.draw()
@@ -91,8 +99,8 @@ class Game:
 
     #collision Logic #5
     def collision(self,x1,y1,x2,y2):
-        if y1>=y2 and y1<=y2+SIZE:
-            if x1>=x2 and x1<=x2+SIZE:
+        if y1>=y2 and y1<y2+SIZE:
+            if x1>=x2 and x1<x2+SIZE:
                 return True
         return False
 
@@ -100,9 +108,20 @@ class Game:
     def play(self):
         self.snake.walk()
         self.apple.draw()
-#5
+        self.scoredisplay()
+        pygame.display.flip()
+#5 snake colliding with apple 
         if self.collision(self.snake.x[0],self.snake.y[0],self.apple.x,self.apple.y): #checking whether the collision happens if happens we check whether the collision of apple and snake head we dont want to check body so x[0],y[0].
+           #7increasing the snake length when snake eats object
+            self.snake.inclength()
             self.apple.move()#6 when collision occurs we move apple to random position ,this method will be called in apple class 
+#8 display score
+    def scoredisplay(self):
+        font=pygame.font.SysFont('vedanta',30) #pygame has font module
+        score=font.render(f"your score: {self.snake.length}",True,(0,0,0))
+        self.surface.blit(score,(1150,10))
+                
+
 
     
 
@@ -126,7 +145,7 @@ class Game:
                 elif event.type==QUIT:
                     running=False
             self.play()#method
-            time.sleep(0.3)
+            time.sleep(0.2)
 
 #game class ends
 
